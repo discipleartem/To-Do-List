d3977f5f204d3ca47f41c69ef807e5075e2b6656
Tomas <magyc@rambler.ru>
2016-02-15 19:57:56 +0200

    done app

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+
+  #Метод для недопущения пустой записи с метода completed в items_controller
+  def completed?
+    !completed_at.blank?
+  end
 end
